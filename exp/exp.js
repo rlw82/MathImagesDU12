3eb1e697a408f469a3d74c3eece79e8a63942ba0
Tyler Romasco <TRomasco@gmail.com>
2012-07-11 14:43:22 -0400

    moved unused libraries to 'old' directory

--- exp/exp.js
+++ exp/exp.js
@@ -2,7 +2,7 @@ var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [0, 5000, 10, 0], axi
 
 JXG.Options.showCopyright = false;
 
-interest = board.create('slider', [[1, 500], [4, 500], [0.05, 0.1, 1]], {name:'Interest Rate'});
+interest = board.create('slider', [[1, 500], [4, 500], [0.05, 001, 1]], {name:'Interest Rate'});
 interval = board.create('slider', [[1, 100], [4, 100], [1, 12, 365]], {name:"Compound Interval"});
 
 curve = board.create('functiongraph', [function(x) {
