641bf21ff4f6252e60f2382786efe8444c8e4369
Tyler Romasco <TRomasco@gmail.com>
2012-07-11 14:33:03 -0400

    cleaned up code

--- exp/exp.js
+++ exp/exp.js
@@ -1,10 +1,12 @@
-var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [0, 2000, 5, 1000], axis: true});      
+var board = JXG.JSXGraph.initBoard('jxgbox', {boundingbox: [0, 5000, 10, 0], axis: true});      
 
-interest = board.create('slider', [[1, 1875], [4, 1875], [0.05, 0.05, 1]], {name:'Interest Rate'});
-interval = board.create('slider', [[1, 1625], [4, 1625], [1, 1, 365]], {name:"Compound Interval"});
+JXG.Options.showCopyright = false;
+
+interest = board.create('slider', [[1, 500], [4, 500], [0.05, 0.1, 1]], {name:'Interest Rate'});
+interval = board.create('slider', [[1, 100], [4, 100], [1, 12, 365]], {name:"Compound Interval"});
 
 curve = board.create('functiongraph', [function(x) {
-                                        return Math.pow(1000 * (1 + (interest.X()/interval.X())), interval.X()*x); 
+                                        return 1000 * Math.pow((1 + (interest.Value()/interval.Value())), interval.Value()*x); 
                                       }]);
 
 
