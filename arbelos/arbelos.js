6c7c770da484a4b5404fcb6ed92381e3ab579470
mutantturkey <mutantturke@gmail.com>
2012-07-12 15:52:17 -0400

    moved math to calculate function, cleaned up declarations, added an intial draw

--- arbelos/arbelos.js
+++ arbelos/arbelos.js
@@ -1,26 +1,11 @@
-function writeMessage(messageLayer, message, message2, xpos, ypos) {
-				var context = messageLayer.getContext();
-				messageLayer.clear();
-				context.font = "18pt Calibri";
-				context.fillStyle = "black";
-				context.fillText(message, xpos, ypos);
-				context.fillText(message2, xpos, ypos + 20);
-}
-
 var padding = 50;
 var rightPadding = 0;
 
 var y = 0;
 
-var right_x = 0;
-var right_r = 0;
-var left_x = 0;
-var left_r = 0;
-
-var right_tangent_x = 0;
-var right_tangent_y = 0;
-var left_tangent_x = 0;
-var left_tangent_y = 0;
+var right_x, right_r, left_x, left_r = 0;
+var right_tangent_x, right_tangent_y, left_tangent_x, left_tangent_y = 0;
+var tangent_line_y1, tangent_line_x1, tangent_line_y2, tangent_line_x2 = 0;
 
 var R = 0;
 var D = 0;
@@ -30,10 +15,40 @@ var left_int_angle = 0;
 
 var slope;
 var intercept;
-var tangent_line_y1 = 0;
-var tangent_line_x1 = 0;
-var tangent_line_y2 = 0;
-var tangent_line_x2 = 0;
+
+function writeMessage(messageLayer, message, message2, xpos, ypos) {
+				var context = messageLayer.getContext();
+				messageLayer.clear();
+				context.font = "18pt Calibri";
+				context.fillStyle = "black";
+				context.fillText(message, xpos, ypos);
+				context.fillText(message2, xpos, ypos + 20);
+}
+
+function calculate() { 
+
+  D = left_x - right_x;
+  R = left_r - right_r;
+
+  right_int_angle =  Math.acos(R/D);
+  left_int_angle =  Math.PI - right_int_angle;
+
+  left_tangent_x = left_x + left_r * Math.cos(-left_int_angle);
+  left_tangent_y = y + left_r * Math.sin(-left_int_angle);
+
+  right_tangent_x = right_x + right_r * Math.cos(Math.PI + right_int_angle);
+  right_tangent_y = y + right_r * Math.sin(Math.PI + right_int_angle);
+
+  slope = (right_tangent_y - left_tangent_y) / (right_tangent_x - left_tangent_x);
+
+  intercept = right_tangent_y - (slope * right_tangent_x);
+  
+  tangent_line_y2 = intercept;
+  tangent_line_x2 = (tangent_line_y2 - intercept) / slope;
+
+  tangent_line_y1 = (slope * 600) + intercept;
+  tangent_line_x1 = (tangent_line_y1 - intercept) / slope;
+}
 
 function drawArbelos(leftArc, rightArc, xpos, arcLayer) {
 
@@ -47,9 +62,9 @@ function drawArbelos(leftArc, rightArc, xpos, arcLayer) {
 
 window.onload = function() {
 				var stage = new Kinetic.Stage({
-container: "container",
-width: 600,
-height: 600
+        container: "container",
+        width: 600,
+        height: 600
 });
 
 width = stage.getWidth();
@@ -96,6 +111,7 @@ left_r = (dragCircle.attrs.x - padding) / 2;
 right_r = (rightPadding - dragCircle.attrs.x) / 2 ;
 right_x = dragCircle.attrs.x + right_r;
 
+
 var leftArc = new Kinetic.Shape({
 	drawFunc: function() {
 		var context = this.getContext(); 
@@ -135,41 +151,20 @@ var rightArc = new Kinetic.Shape({
 });    		
 
 
+calculate();  
+arcLayer.draw();
+
 
 dragCircle.on("dragmove", function() {
 	drawArbelos(leftArc, rightArc, dragCircle.attrs.x, arcLayer);
 	writeMessage(messageLayer, "left radius:" +  (Math.round(left_r)), "right radius:" + (Math.round(right_r)), 10, 25);
-
-  D = left_x - right_x;
-  R = left_r - right_r;
-
-  right_int_angle =  Math.acos(R/D);
-  left_int_angle =  Math.PI - right_int_angle;
-
-  left_tangent_x = left_x + left_r * Math.cos(-left_int_angle);
-  left_tangent_y = y + left_r * Math.sin(-left_int_angle);
-
-  right_tangent_x = right_x + right_r * Math.cos(Math.PI + right_int_angle);
-  right_tangent_y = y + right_r * Math.sin(Math.PI + right_int_angle);
-
-  slope = (right_tangent_y - left_tangent_y) / (right_tangent_x - left_tangent_x);
-
-  intercept = right_tangent_y - (slope * right_tangent_x);
-  
-  tangent_line_y2 = intercept;
-  tangent_line_x2 = (tangent_line_y2 - intercept) / slope;
-
-  tangent_line_y1 = (slope * 600) + intercept;
-  tangent_line_x1 = (tangent_line_y1 - intercept) / slope;
-
+  calculate();
 });
 
 backLayer.add(largeArc);
 dragLayer.add(dragCircle);
 arcLayer.add(rightArc);
 arcLayer.add(leftArc);
-//arcLayer.add(rightAngle);
-//arcLayer.add(leftAngle);
 arcLayer.add(tangentLine);
 stage.add(backLayer);
 stage.add(arcLayer);
