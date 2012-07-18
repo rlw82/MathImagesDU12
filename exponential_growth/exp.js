c59a85b96c8193f73b1c918b186b47246903af53
mutantturkey <mutantturke@gmail.com>
2012-07-18 14:20:12 -0400

    updated exponential growth

--- exponential_growth/exp.js
+++ exponential_growth/exp.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
   interval = board.create('slider', [[1, 250], [2, 250], [1, 1, 25]], {name:"Compound Interval",  snapWidth: 0.5});
   //cont = board.create('slider', [[7, 650], [7, 150], [0, 0, 1]], {spanWidth: 1});
 
+  var yAxis = board.create('text',[-1,1,"Hello World"]); 
   cont = board.create('functiongraph', [function(x) {
     return principal.Value() * Math.pow(Math.E, interest.Value() * x);
   }, 0 ], {strokeColor: 'green'});
