3fb4886d93f21b288c5a7b40fe79cbac4f963542
mutantturkey <mutantturke@gmail.com>
2012-07-24 12:14:07 -0400

    exp updated

--- exponential_growth/exp.js
+++ exponential_growth/exp.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-  var board = JXG.JSXGraph.initBoard('exponential', {boundingbox: [0, 10000, 5, 0], axis: true, showCopyright:false});      
+  var board = JXG.JSXGraph.initBoard('exponential', {boundingbox: [-.5, 10000, 5, -1000], axis: true, showCopyright:false, showNavigation:false});      
 
-  //create('slider', [[startX, startY], [endX, endY], [min, start, max]], {options})
   principal = board.create('slider', [[1, 1050], [2, 1050], [100, 1000, 3000]], {name:'Principal value', snapWidth: 0.5});
   interest = board.create('slider', [[1, 600], [2, 600], [0.05, 0.5, 1]], {name:'Interest Rate'});
-  interval = board.create('slider', [[1, 250], [2, 250], [1, 1, 25]], {name:"Compound Interval",  snapWidth: 0.5});
-  //cont = board.create('slider', [[7, 650], [7, 150], [0, 0, 1]], {spanWidth: 1});
+  interval = board.create('slider', [[1, 250], [2, 250], [1, 1, 25]], {name:"Number of times interest is compounded each year",  snapWidth: 0.5});
 
-  var yAxis = board.create('text',[-1,1,"Hello World"]); 
   cont = board.create('functiongraph', [function(x) {
     return principal.Value() * Math.pow(Math.E, interest.Value() * x);
   }, 0 ], {strokeColor: 'green'});
@@ -19,6 +16,7 @@ $(document).ready(function() {
   curveGlide = board.create('glider', [3, 1000, curve], {withLabel:true, showInfobox:true, name:"Compounded x Times"});
   contGlide = board.create('glider', [3, 1000, cont], {withLabel:true, showInfobox:true, name:"Compunded Continuously"});
 
+  var xAxisText = board.create('text',[1, -600, "Years"]);
   curveGlide.on('drag', function() {
     countGlide.moveTo([curveGlide.X(), Math.pow(Math.E, interest.value() * curveGlide.X())]);
 
